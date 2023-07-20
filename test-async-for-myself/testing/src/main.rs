9719b66541f424ae4badea2a2c757934271bafae
hah-love <legyx1@gmail.com>
2023-07-20 22:43:06 +0300

    hz 2.0

--- test-async-for-myself/testing/src/main.rs
+++ test-async-for-myself/testing/src/main.rs
@@ -1,12 +1,30 @@
-async fn hey() {
-    println!("hello from async");
-}
+// async fn hey() {
+//     println!("hello from async");
+// }
+
+// #[tokio::main]
+// async fn main() {
+//     let op = hey();
+
+//     println!("hello from main!");
+
+//     op.await;
+// }
+
+use tokio::task::yield_now;
+use std::rc::Rc;
 
 #[tokio::main]
 async fn main() {
-    let op = hey();
+    tokio::spawn(async {
+        // The scope forces `rc` to drop before `.await`.
+        {
+            let rc = Rc::new("hello");
+            println!("{}", rc);
+        }
 
-    println!("hello from main!");
-
-    op.await;
+        // `rc` is no longer used. It is **not** persisted when
+        // the task yields to the scheduler
+        yield_now().await;
+    });
 }
